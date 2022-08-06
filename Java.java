19d7bbe612edc247a7ee84597233cbe1096cde50
AlysonCr <99547966+AlysonCr@users.noreply.github.com>
2022-08-06 12:56:40 -0600

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,37 @@
+public class Persona {
+
+    private String nombre;
+    private int edad;
+    public void mostar () {
+
+    }
+
+}
+public class Empleado extends Persona {
+
+    private float sueldo_bruto;
+    
+    public void mostar () {
+
+    }
+    public void Calcular_salario_neto (){
+
+    }
+
+public class directvo extends empleado {
+    private String categoria;
+    
+    public void mostrar (){
+
+    }
+}
+} 
+public class cliente extends Persona {
+    private String nombre_empresa;
+    private int telefono_de_contacto;
+
+    public void mostrar (){
+        
+    }
+
+}
